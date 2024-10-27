1bd6b1ccad8a7f040158547fb68a1324e2b3aa43
Jess JH <89726090+Icelii@users.noreply.github.com>
2024-10-27 16:45:43 -0600

    Migraciones 🌭

--- database/migrations/2024_10_27_223345_instances.php
+++ database/migrations/2024_10_27_223345_instances.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('instances', function(Blueprint $table){
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('id_user')
+            ->constrained('users')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('instances');
     }
 };
